11f418efa40e4e01af3bc62fb1a8ed5a3d82910d
denizsoyhan <oguldenizsoyhan@gmail.com>
2025-03-31 15:34:37 +0300

    added metadata to generated files and naming convention for dowloaded .jsx files with localstorage

--- src/pages/articles/Article2.jsx
+++
@@ -1,9 +0,0 @@
-export default function Article2() {
-    return (
-      <div>
-        <h1>My First Article</h1>
-        <p>This is an examplgsdgsde of an article content.</p>
-      </div>
-    );
-  }
-  
--- src/pages/articles/Article3.jsx
+++
@@ -1,9 +0,0 @@
-export default function Article3() {
-    return (
-      <div>
-        <h1>My First Article</h1>
-        <p>This is an example of an article content.</p>
-      </div>
-    );
-  }
-  
