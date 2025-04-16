74bc3b199901a98c9f2a28984ecafb6d304df0e4
denizsoyhan <oguldenizsoyhan@gmail.com>
2025-04-16 14:15:02 +0300

    only in dev mode customizer and articleGenerator is functional

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,37 @@ import Home from "./pages/Home";
 import ArticlePage from "./pages/Article";
 import BlogHeader from "./BlogHeader";
 import ArticleGenerator from "./ArticleGenerator";
-
-import './index.css'; 
+import './index.css';
 import Customize from "./Customize";
 import BlogFooter from "./BlogFooter";
 
 // import articles with eager loading to get the module content (every article comes as a module)
 const articleModules = import.meta.glob("./pages/articles/*.jsx", { eager: true });
 
-function App() {
+// Check if we're in development mode
+const isDevelopment = import.meta.env.DEV; // Vite provides this variable
 
-  
+function App() {
   return (
     <div className="app">
-
       <BlogHeader></BlogHeader>
-
       <Router>
         <Routes>
           <Route path="/" element={<Home articleModules={articleModules} />} />
-
           {/* Dynamic article route */}
           <Route path="/article/:articleName" element={<ArticlePage articles={articleModules} />} />
-
-          {/* Article Generator */}
-          <Route path="/generate-article" element={<ArticleGenerator />} /> 
-
-          {/*Customizer*/}
-          <Route path="/customize" element={<Customize></Customize>} /> 
+          
+          {/* Only show these routes in development mode */}
+          {isDevelopment && (
+            <>
+              <Route path="/generate-article" element={<ArticleGenerator />} />
+              <Route path="/customize" element={<Customize />} />
+            </>
+          )}
         </Routes>
       </Router>
       <BlogFooter></BlogFooter>
-
     </div>
- 
-    
   );
 }
 
